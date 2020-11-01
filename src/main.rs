ae2a6ac5c7907840a7f4ba9704709977c8216ed2
Dave Allie <dave@daveallie.com>
2020-11-01 12:57:22 +1000

    Add alert notification writing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use chrono::{Local, Timelike};
 use hidapi::HidApi;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
     let api = HidApi::new().expect("Failed to unwarp HID API");
@@ -8,7 +10,11 @@ fn main() {
             d.usage() == 0x0061 && d.usage_page() == 0xFF60).expect("Failed to find device");
     let device = device_info.open_device(&api).expect("Failed to open device");
 
+    // Write time info
     let dt = Local::now();
+    device.write(&[0x00, 0x01, 0x01, dt.hour() as u8, dt.minute() as u8]).expect("Failed to write to device");
+    thread::sleep(Duration::from_millis(100));
 
-    device.write(&[0x00, 0x01, 0x11, dt.hour() as u8, dt.minute() as u8]).expect("Failed to write to device");
+    // Write notification info
+    device.write(&[0x00, 0x02, 0x01, 0]).expect("Failed to write to device");
 }
